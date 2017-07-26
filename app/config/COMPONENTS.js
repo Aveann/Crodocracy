1864f1336570339610379d27a4e78cf1ee0448cf
Aveann <naeva.ar@gmail.com>
2017-07-26 10:06:12 +0200

    paths

--- app/config/COMPONENTS.js
+++ app/config/COMPONENTS.js
@@ -1,6 +1,10 @@
-module.exports = {
-  HeaderGeneral : require('../components/Headers/HeaderGeneral'),
-  HeaderNotifications : require('../components/Headers/HeaderNotifications'),
-  TabBarIcon : require('../components/TabBarIcon'),
+'use strict';
+import React, { Component } from 'react';
 
-};
+var HeaderGeneral = require('../components/Headers/HeaderGeneral');
+var HeaderNotifications = require('../components/Headers/HeaderNotifications');
+
+module.exports = {
+  HeaderGeneral : HeaderGeneral,
+  HeaderNotifications : HeaderNotifications
+}
